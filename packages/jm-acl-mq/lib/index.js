1a0960f4167093e5fbfdc8d86249e98b16e8d6ee
jeff <jeff@jamma.cn>
2020-04-30 09:11:30 +0800

    refactor

--- packages/jm-acl-mq/lib/index.js
+++ packages/jm-acl-mq/lib/index.js
@@ -1,49 +1,44 @@
 const log = require('jm-log4js')
 const logger = log.getLogger('acl-mq')
-const { ms } = require('jm-server')
+const { Service } = require('jm-server')
 
-module.exports = function (opts, app) {
-  ['gateway'].forEach(function (key) {
-    process.env[key] && (opts[key] = process.env[key])
-  })
+class $ extends Service {
+  constructor (opts, app) {
+    super(opts)
+    const { gateway } = opts
+    const { acl } = app.modules
 
-  let o = {
-    ready: true,
-
-    onReady: function () {
-      return this.ready
+    if (!gateway) {
+      logger.warn('no gateway config. I will not work.')
+      return
     }
-  }
 
-  const bind = async (name, uri) => {
-    uri || (uri = '/' + name)
-    o[name] = await ms.client({
-      uri: opts.gateway + uri
+    if (!acl) {
+      logger.warn('no acl module found. I will not work.')
+      return
+    }
+
+    require('./gateway')({ gateway }).then(doc => {
+      doc.bind('mq')
+      this.gateway = doc
+      this.emit('ready')
     })
-  }
 
-  if (!app.modules.acl) {
-    logger.warn('no acl module found. I will not work.')
-    return o
-  }
-  if (!opts.gateway) {
-    logger.warn('no gateway config. I will not work.')
-    return o
-  }
-
-  bind('mq')
-
-  const { acl } = app.modules
-
-  const send = async function (topic, message) {
-    return o.mq.post(`/${topic}`, { message })
-      .catch(e => {
+    const send = async function (topic, message) {
+      await this.onReady()
+      try { await this.gateway.mq.post(`/${topic}`, { message }) } catch (e) {
         logger.error(`send mq fail. topic: ${topic} message: ${JSON.stringify(message)}`)
         logger.error(e)
+      }
+    }
+
+    acl
+      .on('acl.update', opts => {
+        opts && (send('acl.update', opts))
       })
   }
-  acl
-    .on('acl.update', function (opts) {
-      opts && (send('acl.update', opts))
-    })
+}
+
+module.exports = function (opts, app) {
+  return new $(opts, app)
 }
